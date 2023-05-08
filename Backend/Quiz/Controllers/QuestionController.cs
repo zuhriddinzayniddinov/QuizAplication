e335b88acbbb61250d716690e9c5b07654f44ccd
Zayniddinov Zuhriddin <zayniddinovzuhriddin99@gmail.com>
2023-05-08 23:43:22 +0500

    Creted QuestionController and Models

---
+++ Backend/Quiz/Controllers/QuestionController.cs
@@ -0,0 +1,17 @@
+﻿using Domen.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] Question question)
+        {
+
+        }
+    }
+}
