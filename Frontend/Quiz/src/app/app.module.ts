bc6e77e431f59339d79fa26a8e7bccce1b9a4ff4
Zayniddinov Zuhriddin <zayniddinovzuhriddin99@gmail.com>
2023-05-08 16:23:29 +0500

    Added question component

--- Frontend/Quiz/src/app/app.module.ts
+++ Frontend/Quiz/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/Quiz/src/app/question.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    template: '<h1>Bu savol</h1>',
+    selector: 'question'
+})
+export class QuestionComponent {
+
+}
